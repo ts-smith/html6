53f767260a31f50a382e03cee82ceb8868847d10
Trevor <trevorssmith1392@gmail.com>
2013-04-22 22:40:55 -0700

    Fix indents

--- html6.js
+++ html6.js
@@ -12,20 +12,20 @@ angular.module('html6', []).
             template:
                 "<div class=\"navbar navbar-static-top\">" +
                     "<div class=navbar-inner>" +
-                    "<div class=container>" +
-                    "<button type=\"button\" class=\"btn btn-navbar\" data-toggle=collapse data-target=\".nav-collapse\">" +
-                    "<span class=icon-bar></span>" +
-                    "<span class=icon-bar></span>" +
-                    "<span class=icon-bar></span>" +
-                    "</button>" +
-                    "<span class=\"brand\" ng-bind=\"brand\"></span>" +
-                    "<div class=\"nav-collapse collapse\">" +
-                    "<ul class=\"nav\" ng-transclude>" +
-                    "</ul>" +
+                        "<div class=container>" +
+                            "<button type=\"button\" class=\"btn btn-navbar\" data-toggle=collapse data-target=\".nav-collapse\">" +
+                                "<span class=icon-bar></span>" +
+                                "<span class=icon-bar></span>" +
+                                "<span class=icon-bar></span>" +
+                            "</button>" +
+                            "<span class=\"brand\" ng-bind=\"brand\"></span>" +
+                            "<div class=\"nav-collapse collapse\">" +
+                                "<ul class=\"nav\" ng-transclude>" +
+                                "</ul>" +
+                            "</div>" +
+                        "</div>" +
                     "</div>" +
-                    "</div>" +
-                    "</div>" +
-                    "</div>",
+                "</div>",
             replace: true
         };
     }).
@@ -46,14 +46,15 @@ angular.module('html6', []).
             link: function(scope, element, attrs){
                 scope.root = attrs.root;
             },
-            template:   '<div class="tabbable">' +
-                '<ul class="nav nav-tabs">' +
-                '<li ng-repeat="pane in panes" ng-init="pane.index=$index;" ng-class="{active: tab==$index+1}">' +
-                '<a href="#{{root}}/{{$index + 1}}">{{pane.title}}</a>' +
-                '</li>' +
-                '</ul>' +
-                '<div class="tab-content" ng-transclude>' +
-                '</div>' +
+            template:
+                '<div class="tabbable">' +
+                    '<ul class="nav nav-tabs">' +
+                        '<li ng-repeat="pane in panes" ng-init="pane.index=$index;" ng-class="{active: tab==$index+1}">' +
+                            '<a href="#{{root}}/{{$index + 1}}">{{pane.title}}</a>' +
+                        '</li>' +
+                    '</ul>' +
+                    '<div class="tab-content" ng-transclude>' +
+                    '</div>' +
                 '</div>',
             replace: true
         };
